02209cfe6a19a9be1c808a1e4d64653ca0af3f02
MadSpun4 <artem-polyakov-04@bk.ru>
2026-04-21 13:47:41 +1000

    Add some fixes

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM postgres:13
 
+USER postgres
+
 COPY init.sql /docker-entrypoint-initdb.d/01-init.sql
--- init.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER castrated_user WITH PASSWORD 'root' NOSUPERUSER NOCREATEDB NOCREATEROLE;
-
-CREATE DATABASE castrated_user_db OWNER castrated_user;
-
-GRANT CONNECT ON DATABASE castrated_user_db TO castrated_user;
